f96f8035d1f68d7653ff5fdf23d4a2defffb50d8
KuZJr <ikaru1999@gmail.com>
2020-01-30 12:32:31 +0100

    TP1 advanced all pages

--- advanced/app.js
+++ advanced/app.js
@@ -2,20 +2,32 @@ const axios = require("axios");
 const myclass = require("./planet");
 // https://swapi.co/api/planets
 let planets = [];
+let url = 'https://swapi.co/api/planets';
+let page = true;
 
-axios.get('https://swapi.co/api/planets')
-.then(function (response) {
-    for (let i = 0; i < response.data.results.length; ++i) {
-        let planet = new myclass.Planet(response.data.results[i]);
-        planets.push(planet);
+async function SWPop() {
+    while (page == true) {
+        await axios.get(url)
+        .then(function (response) {
+            for (let i = 0; i < response.data.results.length; ++i) {
+                let planet = new myclass.Planet(response.data.results[i]);
+                planets.push(planet);
+            }
+            if (response.data.next != null)
+                url = response.data.next;
+            else
+                page = false;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     }
-    
+
     for (let i = 0; i < planets.length; ++i) {
         console.log(`Planet "${planets[i].name}" - Population : ${planets[i].population}`);
     }
     
     console.log("Total population : " + myclass.Planet.countPlanetsPopulation(planets));
-})
-.catch(function (error) {
-    console.log(error);
-});
+}
+
+SWPop();
